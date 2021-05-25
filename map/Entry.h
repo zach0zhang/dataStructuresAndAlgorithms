bd4dd45ce2db62a0d161bc3c0da12a76b2559daa
zach <584191872@qq.com>
2021-05-25 22:19:45 +0800

    refactor: move the Entry class to map folder

---
+++ map/Entry.h
@@ -0,0 +1,21 @@
+#ifndef __ENTRY_H__
+#define __ENTRY_H__
+
+template <typename K, typename V>
+class Entry {
+public:
+    typedef K Key;
+    typedef V Value;
+public:
+    Entry(const K& k = K(), const V& v = V()) : _key(k), _value(v) { }
+    const K& key() const { return _key; }
+    const V& value() const { return _value; }
+    void setKey(const K& k) { _key = k; }
+    void setValue(const V& v) { _value = v; }
+private:
+    K _key;
+    V _value;
+};
+
+
+#endif
--- tree/SearchTree.h
+++ tree/SearchTree.h
@@ -1,4 +1,5 @@
 #include "LinkedBinaryTree.h"
+#include "Entry.h"
 
 #include <stdexcept>
 #include <string>
@@ -10,21 +11,6 @@ public:
     NonexistentElement(const string& err) : runtime_error(err) { }
 };
 
-template <typename K, typename V>
-class Entry {
-public:
-    typedef K Key;
-    typedef V Value;
-public:
-    Entry(const K& k = K(), const V& v = V()) : _key(k), _value(v) { }
-    const K& key() const { return _key; }
-    const V& value() const { return _value; }
-    void setKey(const K& k) { _key = k; }
-    void setValue(const V& v) { _value = v; }
-private:
-    K _key;
-    V _value;
-};
 
 template <typename E>
 class SearchTree {
